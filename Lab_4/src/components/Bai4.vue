46eb94d72b818cc4568536cf2910e6f6320eb501
thpbmt <you@example.com>
2026-01-22 09:07:49 +0700

    Push lab 4

---
+++ Lab_4/src/components/Bai4.vue
@@ -0,0 +1,54 @@
+<template lang="">
+    <div class="container mt-5">
+<h1 class="text-center">Kiến thức sức khỏe cộng đồng</h1>
+<div class="row">
+<div class="col-sm-4">
+<div class="card">
+<img :src="items[0].image" alt="Hình ảnh" />
+<div class="card-body">
+<h3 class="card-title">{{ items[0].title }}</h3>
+<p class="card-text">{{ items[0].content }}</p>
+<button class="btn btn-info">Xem chi tiết </button>
+</div>
+</div>
+</div>
+<div class="col-sm-4">
+<div class="card">
+<img :src="items[1].image" alt="Hình ảnh" />
+<div class="card-body">
+<h3 class="card-title">{{ items[1].title }}</h3>
+<p class="card-text">{{ items[1].content }}</p>
+<button class="btn btn-info">Xem chi tiết </button>
+</div>
+</div>
+</div>
+<div class="col-sm-4">
+<div class="card">
+<img :src="items[2].image" alt="Hình ảnh" />
+<div class="card-body">
+<h3 class="card-title">{{ items[2].title }}</h3>
+<p class="card-text">{{ items[2].content }}</p>
+<button class="btn btn-info">Xem chi tiết </button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</template>
+<script setup>
+// Import hình ảnh từ thư mục assets
+import img1 from '../assets/images/banner1.jpg';
+import img2 from '../assets/images/banner2.jpg';
+import img3 from '../assets/images/banner3.jpg';
+const items = ([
+{ title: '8 loại rau củ quả giàu canxi', content: 'Canxi là...', image: img1
+},
+{ title: 'Các loại gia vị tốt cho sức khỏe', content: 'Một số loại...',
+image: img2 },
+{ title: '9 loại đậu bổ dưỡng nên dùng nhiều', content: 'Đậu lăng...', image:
+img3 },
+]);
+</script>
+<style lang="">
+
+</style>
---
+++ Lab_4/src/components/tet.vue
@@ -0,0 +1,31 @@
+<template lang="">
+  <div>
+    <button v-on:click="changeAhihi" v-html="title"> </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title : "hide"
+    };
+  },
+
+  methods: {
+    changeAhihi() {
+      console.log(this.title);
+
+      if (this.title === "hide") {
+        this.title = "show";
+        return;
+      }
+
+      this.title = "hide";
+    }
+  }
+}
+</script>
+
+<style lang="">
+</style>
